81568571b0ccdeb292364a38768715ea1a6bf4a9
Margot Desmet <margotdesmet@hotmail.com>
2020-03-22 17:55:01 +0100

    nav dropdown (min auto close)

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var list = document.querySelector(".list-button");
 
 list.addEventListener("click", function(){
     document.querySelector(".absolute-list").style.display="block";
+    document.querySelector(".absolute-popup").style.display="none";
+    document.querySelector(".absolute-overlay").style.display="none";
 });
 
 var map = document.querySelector(".map-button");
@@ -15,4 +17,36 @@ var popup = document.querySelector(".popup-more");
 popup.addEventListener("click", function(){
     document.querySelector(".absolute-popup").style.display="none";
     document.querySelector(".absolute-overlay").style.display="block";
+});
+
+var closePopup = document.querySelector(".close-popup");
+
+closePopup.addEventListener("click", function(){
+    document.querySelector(".absolute-popup").style.display="none";
+});
+
+var closeOverlay = document.querySelector(".close-overlay");
+
+closeOverlay.addEventListener("click", function(){
+    document.querySelector(".absolute-overlay").style.display="none";
+});
+
+var closeList = document.querySelector(".close-list");
+
+closeList.addEventListener("click", function(){
+    document.querySelector(".absolute-list").style.display="none";
+});
+
+
+
+$(document).ready(function(){
+    $(".nav-link").click(function(){ 
+        var navLink = $(this).attr("id");
+        if($("." + navLink + ":visible").length){;
+          $("." + navLink).slideUp();
+        } else {
+          $(".dropdown").slideUp();
+          $("." + navLink).slideDown();  
+        }
+    });
 });
