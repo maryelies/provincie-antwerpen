63fa469d5337d041418a98010135257717476925
Margot Desmet <margotdesmet@hotmail.com>
2020-03-23 21:53:03 +0100

    delete script

--- js/script.js
+++
@@ -1,9 +0,0 @@
-( function() {
-    $ = jQuery;
-
-    $(document).ready(function(){
-        $(".chosen-select").chosen({
-            placeholder_text_single: "Zoek op trefwoord..."
-          });
-    });
-  } )();
