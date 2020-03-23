f215098dc2635296b5897623c11b718f34be23fd
Emily <emily.berghen@hotmail.com>
2020-03-24 00:19:00 +0100

    menu

--- js/main.js
+++ js/main.js
@@ -46,11 +46,11 @@ $(document).ready(function(){
     (function () {
         $('.hamburger-wrapper').on('click', function() {
             $('.hamburger-menu').toggleClass('animate');
-            $('.mobile-menu-overlay').toggleClass('visible');
+            $('.navbar').toggleClass('visible');
         })
         $('.mobile-menu-overlay > ul > li > a').on('click', function () {
             $('.hamburger-menu').removeClass('animate');
-            $('.mobile-menu-overlay').removeClass('visible');
+            $('.navbar').removeClass('visible');
         })
     })();
 
